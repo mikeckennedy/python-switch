10d259e8add96d3e686baf6ed2b0145ff1fbdc46
Ian Epperson <ian.epperson@motionloft.com>
2017-09-14 21:11:05 -0700

    Adds ability to specify a fallthrough

--- switchlang.py
+++ switchlang.py
@@ -1,29 +1,38 @@
 # Here is a first pass implementation at adding switch
 import uuid
-from typing import Callable, Any, List
+from typing import Callable, Any
 
 
 class switch:
     __no_result = uuid.uuid4()
+    __default = uuid.uuid4()
 
     def __init__(self, value):
         self.value = value
-        self.cases = {}
+        self.cases = set()
+        self._found = False
         self.__result = switch.__no_result
+        self._falling_through = False
+        self._func_stack = []
 
     def default(self, func: Callable[[], Any]):
-        self.case('__default__', func)
+        self.case(switch.__default, func)
 
-    def case(self, key, func: Callable[[], Any]):
-        if isinstance(key, range):
-            for n in key:
-                self.case(n, func)
-            return
+    def case(self, key, func: Callable[[], Any], fallthrough=False):
+        if fallthrough is not None:
+            if self._falling_through:
+                self._func_stack.append(func)
+                if not fallthrough:
+                    self._falling_through = False
 
-        if isinstance(key, list):
+        if isinstance(key, list) or isinstance(key, range):
+            found = False
             for i in key:
-                self.case(i, func)
-            return
+                if self.case(i, func, fallthrough=None):
+                    found = True
+                    if fallthrough is not None:
+                        self._falling_through = fallthrough
+            return found
 
         if key in self.cases:
             raise ValueError("Duplicate case: {}".format(key))
@@ -32,7 +41,13 @@ class switch:
         if not callable(func):
             raise ValueError("Func must be callable.")
 
-        self.cases[key] = func
+        self.cases.add(key)
+        if key == self.value or not self._found and key == self.__default:
+            self._func_stack.append(func)
+            self._found = True
+            if fallthrough is not None:
+                self._falling_through = fallthrough
+            return True
 
     def __enter__(self):
         return self
@@ -41,21 +56,19 @@ class switch:
         if exc_val:
             raise exc_val
 
-        func = self.cases.get(self.value)
-        if not func:
-            func = self.cases.get('__default__')
-
-        if not func:
+        if not self._func_stack:
             raise Exception("Value does not match any case and there "
                             "is no default case: value {}".format(self.value))
 
-        # noinspection PyCallingNonCallable
-        self.__result = func()
+        for func in self._func_stack:
+            # noinspection PyCallingNonCallable
+            self.__result = func()
 
     @property
     def result(self):
         if self.__result == switch.__no_result:
-            raise Exception("No result has been computed (did you access switch.result inside the with block?)")
+            raise Exception("No result has been computed (did you access "
+                            "switch.result inside the with block?)")
 
         return self.__result
 
